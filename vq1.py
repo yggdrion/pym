8f17747d229497c16b9c815e966ad7ad2ec32d33
r4b2 <github@r4b2.de>
2023-08-09 15:09:21 +0200

    add valve query sample

---
+++ vq1.py
@@ -0,0 +1,5 @@
+from sourceserver.sourceserver import SourceServer
+
+srv = SourceServer("45.145.224.34:27165")
+
+print(srv.info)
---
+++ vq2.py
@@ -0,0 +1,9 @@
+import a2s
+
+address = ("45.145.224.34", 27165)
+
+info = a2s.info(address)
+
+print(info)
+
+print(info.server_name)
