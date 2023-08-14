7f53f6ae0b56fdc567df807a1d422559a5fe4d80
r4b2 <github@r4b2.de>
2023-08-15 01:12:30 +0200

    add additional info

--- squad.py
+++ squad.py
@@ -4,9 +4,10 @@ from concurrent.futures import ThreadPoolExecutor
 import time
 from src import influx
 
-def a2s_info(host, port):
+def a2s_info(name, host, port):
     return_dict = {}
     try:
+        #print(f"Getting info for {name} - {host}:{port}")
         address = (host, port)
         info = a2s.info(address, timeout=10)
         # print(f"Got info for {host}:{port}")
@@ -22,8 +23,18 @@ def a2s_info(host, port):
         #for player in players:
             #print(player.name)
 
+        return_dict['name'] = name
         return_dict['server_name'] = info.server_name
         return_dict['player_count'] = info.player_count
+        return_dict['map_name'] = info.map_name
+        return_dict['game_mode'] = rules['GameMode_s']
+        return_dict['team_one'] = rules['TeamOne_s']
+        return_dict['team_two'] = rules['TeamTwo_s']
+        
+        # for i in return_dict:
+        #     print(f"{i}: {return_dict[i]}")
+        # print("#######################")        
+
         return return_dict
     
     except:
@@ -45,17 +56,18 @@ if __name__ == "__main__":
         print("Squad: Thread")
         with ThreadPoolExecutor(max_workers=3) as executor:
             for server in server_list:
-                thread_return = executor.submit(a2s_info, server['ip'], server['port'])
+                thread_return = executor.submit(a2s_info, server['name'], server['ip'], server['port'])
                 if thread_return.result() is None:
                     print(f"Error: {server['name']}")
                     continue
-                tmp_dict = thread_return.result()
-                tmp_dict['name'] = server['name']
-                squad_data.append(tmp_dict)
+                squad_data.append(thread_return.result())
 
         print("Squad: Influx")
         for data in squad_data:
             #print(data)
-            influx.write('squad', {'name': data['name']}, {'player_count': data['player_count']})
+            measurement = 'squad'
+            tags = {'name': data['name']}
+            fields = {'player_count': data['player_count']}
+            influx.write(measurement, tags, fields)
             
         time.sleep(30)
